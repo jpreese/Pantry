f427dc67d03ab32661346893f888c54bcc39972b
John Reese <jpreese@mtu.edu>
2018-02-27 23:42:06 -0400

    Initial commit

---
+++ Pantry/App/pantryApp.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('pantryModule', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when('/login',
+    {
+        templateUrl: '/App/Partials/Login.html'
+    })
+    .otherwise(
+    {
+        redirectTo: '/'
+    });
+
+});

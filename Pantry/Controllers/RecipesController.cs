fffe00a7900a7e763466dbcc41d539ad32b914e5
John Reese <jpreese@mtu.edu>
2018-03-10 04:52:26 -0400

    Add GetRecipe action

--- Pantry/Controllers/RecipesController.cs
+++ Pantry/Controllers/RecipesController.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Http;
-using System.Web.Mvc;
+using Pantry.Context;
 
 namespace Pantry.Controllers
 {
-    public class RecipesController
+    public class RecipesController : ApiController
     {
+        private RecipesContext db = new RecipesContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        public IHttpActionResult GetRecipe(int id)
+        {
+            var recipe = db.Recipe.FirstOrDefault(r => r.RecipeId == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(recipe);
+        }
     }
 }

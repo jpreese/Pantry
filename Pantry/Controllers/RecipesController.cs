947b6e56273c1a4556ba3abf1883b547047e8019
John Reese <jpreese@mtu.edu>
2018-03-10 02:30:00 -0400

    Initial commit

---
+++ Pantry/Controllers/RecipesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Pantry.Controllers
+{
+    public class RecipesController
+    {
+    }
+}

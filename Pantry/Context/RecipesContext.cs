11bed605f382c6b5aee4b3446cb944e07325f5a1
John Reese <jpreese@mtu.edu>
2018-03-10 01:54:08 -0400

    Add new Context and Model files for use in Entity

---
+++ Pantry/Context/RecipesContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Pantry.Models;
+
+namespace Pantry.Context
+{
+    public class RecipesContext : DbContext
+    {
+        public DbSet<Recipe> Recipe { get; set; }
+
+        public DbSet<Ingredient> Ingredient { get; set; }
+
+        public DbSet<RecipeIngredient> RecipeIngredient { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Pantry/Models/Ingredient.cs
@@ -0,0 +1,8 @@
+﻿namespace Pantry.Models
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public string Name { get; set; }
+    }
+}

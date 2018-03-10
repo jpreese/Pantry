f564d0864065fbb7cc91d58139bfc712985161de
John Reese <jpreese@mtu.edu>
2018-03-09 23:56:23 -0400

    Add new Recipes context

---
+++ Pantry/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pantry.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Ingredients { get; set; } 
+    }
+}

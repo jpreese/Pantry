38306aeeff1bd505040b585e5d9b26e4889ffd05
John Reese <jpreese@mtu.edu>
2018-03-10 21:23:27 -0400

    Initial commit

---
+++ Pantry/AuthRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Pantry.Context;
+using Pantry.Models;
+
+namespace Pantry
+{
+    public class AuthRepository
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthRepository()
+        {
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(new AuthContext()));
+        }
+
+        public async Task<IdentityResult> RegisterUser(User userModel)
+        {
+            var user = new IdentityUser
+            {
+                UserName = userModel.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(User userModel)
+        {
+            var user = await _userManager.FindAsync(userModel.UserName, userModel.Password);
+
+            return user;
+        }
+    }
+}
---
+++ Pantry/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Pantry.Models;
+
+namespace Pantry.Controllers
+{
+    public class AccountController : ApiController
+    {
+        private readonly AuthRepository _authRepository;
+
+        public AccountController()
+        {
+            _authRepository = new AuthRepository();
+        }
+
+        public async Task<IHttpActionResult> Register(User userModel)
+        {
+            var result = await _authRepository.RegisterUser(userModel);
+
+            return Ok();
+        }
+    }
+}

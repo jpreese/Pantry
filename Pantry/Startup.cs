3753ff6fd58784a191cf2316c95e93fa1147dfa3
John Reese <jpreese@mtu.edu>
2018-03-14 21:56:23 -0400

    Initial commit

---
+++ Pantry/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Pantry;
+using Pantry.Providers;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace Pantry
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            var serverOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new AuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(serverOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}

9943a8fdac63867afe6f5d3fbbbabe84923a9a4d
John Reese <jpreese@mtu.edu>
2018-03-16 00:32:24 -0400

    Initial login and registration

--- Pantry/app/pantryApp.js
+++ Pantry/app/pantryApp.js
@@ -1,28 +1,29 @@
-﻿(function() {
+﻿var app = angular.module("pantryModule", ["ngRoute", "LocalStorageModule"]);
 
-    var app = angular.module("pantryModule", ["ngRoute"]);
+app.config(function($routeProvider, $locationProvider) {
 
-    app.config(function($routeProvider, $locationProvider) {
-
-        var partialsDirectoryBase = "/app/partials/";
-        $routeProvider.when("/",
-        {
-            templateUrl: partialsDirectoryBase + "login.html"
-        })
-        .when("/kitchen",
-        {
-            templateUrl: partialsDirectoryBase + "kitchen.html"
-        })
-        .otherwise(
-        {
-            redirectTo: "/login"
-        });
-
-        $locationProvider.html5Mode(
-        {
-            enabled: true,
-            requireBase: false
-        });
+    var partialsDirectoryBase = "/app/partials/";
+    $routeProvider.when("/",
+    {
+        controller: "loginController",
+        templateUrl: partialsDirectoryBase + "login.html"
+    })
+    .when("/kitchen",
+    {
+        templateUrl: partialsDirectoryBase + "kitchen.html"
+    })
+    .otherwise(
+    {
+        redirectTo: "/login"
     });
 
-})();
+    $locationProvider.html5Mode(
+    {
+        enabled: true,
+        requireBase: false
+    });
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);

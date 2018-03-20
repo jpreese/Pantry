1f809db151946715df09957bc5dd8ad5040c4e60
John Reese <jpreese@mtu.edu>
2018-03-20 03:10:34 -0400

    Merge config and app into single file

--- Pantry.Web/app/app.js
+++ Pantry.Web/app/app.js
@@ -3,5 +3,38 @@
 
     angular.module('app', ['ngRoute', 'LocalStorageModule']);
 
+    angular.module('app').config(function ($routeProvider, $httpProvider) {
+
+        var partialsDirectoryBase = '/app/partials/';
+        $routeProvider.when('/login',
+            {
+                controller: 'loginController',
+                templateUrl: partialsDirectoryBase + "login.html"
+            })
+            .when('/pantry',
+            {
+                templateUrl: partialsDirectoryBase + "pantry.html"
+            })
+            .when('/recipes',
+            {
+                controller: 'recipesController',
+                templateUrl: partialsDirectoryBase + "recipes.html"
+            })
+            .otherwise(
+            {
+                redirectTo: '/login'
+            });
+
+        $httpProvider.interceptors.push('authInterceptorService');
+    });
+
+    angular.module('app').run([
+        'authService', function (authService) {
+            authService.fillAuthData();
+        }
+    ]);
+
+    angular.module('app').constant('serviceBase', 'http://localhost:18396/');
+
 })();
 

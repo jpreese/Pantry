1397c509734c3b1cfc0a10f81f8962ccaed9b23b
John Reese <jpreese@mtu.edu>
2018-03-30 18:55:19 -0400

    Initial commit

---
+++ Pantry.Api/Models/UserIngredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Pantry.Models
+{
+    public class UserIngredient
+    {
+        public int UserIngredientId { get; set; }
+        public int IngredientId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+        public Ingredient Ingredient { get; set; }
+    }
+}

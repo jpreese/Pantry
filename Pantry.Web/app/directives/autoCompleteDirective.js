825d0a1be9cfe094dce0aa771d15856153ce4c9a
John Reese <jpreese@mtu.edu>
2018-04-21 18:46:04 -0500

    Refactor out directive to own file

---
+++ Pantry.Web/app/directives/autoCompleteDirective.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').directive('autoComplete', [
+        'ingredientsService', function (ingredientsService) {
+
+            return {
+                link: function (scope, elem, attr, ctrl) {
+                    elem.autocomplete({
+                        source: ingredientsService.getIngredients()
+                    });
+                }
+            };
+
+        }
+    ]);
+})();
